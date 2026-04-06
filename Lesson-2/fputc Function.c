a104ee6837cb1953c47d5b78612a559444c05e9e
YigitErzen <yigterzen@gmail.com>
2026-04-06 14:36:36 +0300

    Refactor code structure for improved readability and maintainability

---
+++ Lesson-2/fputc Function.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+    char data[] = "Hello World!";
+    FILE *fp = fopen("output.txt", "w");
+    if(fp == NULL)
+    {
+        perror("Error opening file");
+    }
+    else
+    {
+        /*printf("%c",fputc('A', fp));
+        fputc(66, fp);
+        fputc('C', fp);
+        fclose(fp);*/
+
+        int index = 0; 
+        char ch = '\0';
+        while (data[index] != '\0'){
+            ch = fputc(data[index], fp);
+            printf("%c", ch);
+            index++;
+        }
+    }
+    return 0;
+}
